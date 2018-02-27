08c34b5f3bfee8280739307adb48c0c7fc42e70f
Mauricio Leal <maltron@gmail.com>
2018-02-27 14:35:01 -0300

    Added some default values when they are missing

--- src/main/java/net/nortlam/research/setup/MongoProvider.java
+++ src/main/java/net/nortlam/research/setup/MongoProvider.java
@@ -6,11 +6,6 @@ import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.IndexModel;
-import com.mongodb.client.model.IndexOptions;
-import com.mongodb.client.model.Indexes;
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
@@ -19,7 +14,6 @@ import javax.ejb.ConcurrencyManagementType;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
-import net.nortlam.research.model.Person;
 
 /**
  *
@@ -29,6 +23,13 @@ import net.nortlam.research.model.Person;
 public class MongoProvider {
 
     private static final Logger LOG = Logger.getLogger(MongoProvider.class.getName());
+    
+    // For Development Purposes only
+    public static final String DEFAULT_HOSTNAME="localhost";
+    public static final String DEFAULT_SERVICE_PORT="27017";
+    public static final String DEFAULT_DB_USERNAME="mauricio";
+    public static final String DEFAULT_DB_PASSWORD="maltron";
+    public static final String DEFAULT_DB_DATABASE="hellomongo";
     
     private MongoClient client;
     private String database;
@@ -46,12 +47,18 @@ public class MongoProvider {
     
     private ServerAddress serverAddress() {
         String hostname = System.getenv("DB_SERVICE_HOSTNAME");
-        if(hostname == null) 
-            LOG.log(Level.SEVERE, "### MongoDB: variable DB_SERVICE_HOSTNAME is not set");
+        if(hostname == null) {
+            LOG.log(Level.SEVERE, "### MongoDB: variable DB_SERVICE_HOSTNAME"
+                    + " is not set. Assuming Default value \"{0}\"", DEFAULT_HOSTNAME);
+            hostname = DEFAULT_HOSTNAME;
+        }
         
         String portString = System.getenv("DB_SERVICE_PORT");
-        if(portString == null)
-            LOG.log(Level.SEVERE, "### MongoDB: variable DB_SERVICE_PORT is not set");
+        if(portString == null) {
+            LOG.log(Level.SEVERE, "### MongoDB: variable DB_SERVICE_PORT "
+                    + "is not set. Assuming Default value \"{0}\"", DEFAULT_SERVICE_PORT);
+            portString = DEFAULT_SERVICE_PORT;
+        }
         
         int port = 0;
         try {
@@ -65,16 +72,25 @@ public class MongoProvider {
     
     private MongoCredential credentials() {
         String username = System.getenv("DB_USERNAME");
-        if(username == null) 
-            LOG.log(Level.SEVERE, "### MongoDB: variable DB_USERNAME is not set");
+        if(username == null) {
+            LOG.log(Level.SEVERE, "### MongoDB: variable DB_USERNAME is not set. "
+                    + "Assuming Default value \"{0}\"", DEFAULT_DB_USERNAME);
+            username = DEFAULT_DB_USERNAME;
+        }
         
         String password = System.getenv("DB_PASSWORD");
-        if(password == null) 
-            LOG.log(Level.SEVERE, "### MongoDB: variable DB_PASSWORD is not set");
+        if(password == null) {
+            LOG.log(Level.SEVERE, "### MongoDB: variable DB_PASSWORD is not set. "
+                    + "Assuming Default value \"{0}\"", DEFAULT_DB_PASSWORD);
+            password = DEFAULT_DB_PASSWORD;
+        }
         
         database = System.getenv("DB_DATABASE");
-        if(database == null)
-            LOG.log(Level.SEVERE, "### MongoDB: variable DB_DATABASE is not set");
+        if(database == null) {
+            LOG.log(Level.SEVERE, "### MongoDB: variable DB_DATABASE is not set. "
+                    + "Assuming Default value \"{0}\"", DEFAULT_DB_DATABASE);
+            database = DEFAULT_DB_DATABASE;
+        }
         
         return MongoCredential.createCredential(username, database, password.toCharArray());
     }
