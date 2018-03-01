0d10f63c778b8f201c7b19d688156849a43763dc
Mauricio Leal <maltron@gmail.com>
2018-03-01 19:09:18 -0300

    Added a URLSession example

--- ios/hellomongo/PeopleViewController.swift
+++ ios/hellomongo/PeopleViewController.swift
@@ -34,35 +34,91 @@ class PeopleViewController: UITableViewController, NSFetchedResultsControllerDel
     
     var fetchedResultsController: NSFetchedResultsController<Person>!
     
+    var dataTask: URLSessionDataTask!
+    
     // TEST TEST TEST TEST TEST TEST
     @objc func handleTest() {
         
-        let url: URL = URL(string: "http://hellomongo-app-hellomongo.cloudapps.maltron.solutionarchitectsredhat.com.br/api/v1/person")!
+        navigationItem.leftBarButtonItem?.isEnabled = false
+        navigationItem.rightBarButtonItem?.isEnabled = false
         
-        if let json: Data = performRequest(url: url) {
-            do {
-                let decoder = JSONDecoder()
-                let result = try decoder.decode([PersonResult].self, from: json)
-                for personResult in result {
-                    print(">>> \(personResult.firstName) \(personResult.lastName)")
+//        let queue = DispatchQueue.global()
+//        queue.async {
+            // Step 1/5: Create a URL object just like before
+            let url: URL = URL(string: "http://hellomongo.cloudapps.nortlam.net/api/v1/person")!
+            // Step 2/5 Get a shared URLSession, using default configuration
+            //          with respect to caching, cookies, and other web stuff
+            let session: URLSession = URLSession.shared
+            // Step 3/5 Data Tasks are for fetching the contents of a given URL
+            //          the code form the completion handler will be invoked when the data
+            //          has received a response from the server
+            dataTask = session.dataTask(with: url, completionHandler: { (data, response, error) in
+                print("On main thread ? " + (Thread.current.isMainThread ? "Yes" : "No"))
+                
+                // Step 4/5 data, response and error are all optionals.
+                //          if error is nil, the communication with the server succeeded
+                //          response holds the server's response code and headers
+                //          data contain the actual data fetched from the server
+                if let _ = error {
+                    //print("FAILURE! \(error)")
+                    DispatchQueue.main.async {
+                        self.showNetworkError()
+                    }
+                } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                    print("SUCCESS!")
+                    if let json: Data = data {
+                        do {
+                            let decoder: JSONDecoder = JSONDecoder()
+                            let result: [PersonResult] = try decoder.decode([PersonResult].self, from: json)
+                            for personResult in result {
+                                print(">>> \(personResult.firstName) \(personResult.lastName)")
+                            }
+                        } catch let decodeErr {
+                            print("### DECODING FAILED:", decodeErr)
+                        }
+                    }
                 }
-            } catch let decodeErr {
-                print("### handleTest() UNABLE TO DECODE:", decodeErr)
-            }
-        }
+                
+                DispatchQueue.main.async {
+                    self.navigationItem.leftBarButtonItem?.isEnabled = true
+                    self.navigationItem.rightBarButtonItem?.isEnabled = true
+                }
+                
+            })
         
-        
+        // Step 5/5 Once you created data task, you call resume to start it
+        //          This sends the request to the server on a background thread
+        //
+            dataTask.resume()
+            
+//            if let json: Data = self.performRequest(url: url) {
+//                do {
+//                    let decoder = JSONDecoder()
+//                    let result = try decoder.decode([PersonResult].self, from: json)
+//                    for personResult in result {
+//                        print(">>> \(personResult.firstName) \(personResult.lastName)")
+//                    }
+//                } catch let decodeErr {
+//                    print("### handleTest() UNABLE TO DECODE:", decodeErr)
+//                }
+//            }
+            
+//            DispatchQueue.main.async {
+//                self.navigationItem.leftBarButtonItem?.isEnabled = true
+//                self.navigationItem.rightBarButtonItem?.isEnabled = true
+//            }
+//        }
     }
     
-    private func performRequest(url: URL) -> Data? {
-        do {
-            return try Data(contentsOf: url)
-        } catch let performErr {
-            print("### performRequest() UNABLE TO FETCH:", performErr)
-        }
-        
-        return nil
-    }
+//    private func performRequest(url: URL) -> Data? {
+//        do {
+//            return try Data(contentsOf: url)
+//        } catch let performErr {
+//            print("### performRequest() UNABLE TO FETCH:", performErr)
+//        }
+//
+//        return nil
+//    }
     
     private func showNetworkError() {
         let alert = UIAlertController(title: "Whoops...", message: "There was an error accessing the iTunes Store", preferredStyle: .alert)
